fd89bd015a062f1df6be1cfac985e0f83316ded5
Leon Raj <leonraj1606@gmail.com>
2023-09-02 10:18:34 -0700

    Added the Object class and added method to keep foreground objects constantly moving.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ pygame.init()
 clock = pygame.time.Clock()
 display_width = 1280
 display_height = 720
-screen = pygame.display.set_mode((display_width, display_height))
+screen = pygame.display.set_mode((display_width, display_height),depth = 8)
 
 class Background():
     def __init__(self, folder_location, num_of_layers, y_offset=0, repetition=2, scale=(1.0, 1.0)):
@@ -22,7 +22,6 @@ class Background():
             self.layers.append(layer)
 
         self.layer_width = self.layers[0].get_width()
-        print(self.layer_width, self.layers[0].get_height())
         self.layer_height = display_height - self.layers[0].get_height() - y_offset   #This does not represent the actual height of the layer but rather the y-coordinate of the top-left corner.
         self.left_edge = -(self.layer_width * self.repetition) / (1 + self.speed_var)
         self.right_edge = (self.layer_width * self.repetition) / (1 + self.speed_var)
@@ -83,6 +82,29 @@ class SpriteSheet():
         temp = pygame.Surface.copy(frame)
         return temp
 
+    def join(self, orientation, sprite_locations):                  #sprite_locations is a list containing row, and column number of each sprite as a tuple.
+        frame = None
+        if orientation == 'H':                                      #orientation can either be 'H' for horizontal join or 'V' for vertical join.
+            frame_width = self.sprite_width * len(sprite_locations)
+            frame = pygame.Surface((frame_width, self.sprite_height))
+            frame.fill((255, 255, 255))
+            for i, location in enumerate(sprite_locations):
+                sprite = self.get_sprite(location[0],location[1])
+                frame.blit(sprite, (self.sprite_width * i, 0))
+
+        elif orientation == 'V':
+            frame_height = self.sprite_height * len(sprite_locations)
+            frame = pygame.Surface((self.sprite_width, frame_height))
+            frame.fill((255, 255, 255))
+            for i, location in enumerate(sprite_locations):
+                sprite = self.get_sprite(location[0], location[1])
+                frame.blit(sprite, (0, self.sprite_height * i))
+
+        frame.set_colorkey((255, 255, 255))
+        temp = frame.copy()
+        return temp
+
+
 
 
 class Foreground():
@@ -90,19 +112,22 @@ class Foreground():
 
         self.dx = dx
         self.dy = dy
-        self.blit_list=[]
+        self.tile_width = tile_width
+        self.tile_height = tile_height
+        self.movable_objects = []
+        self.move_infos = []
+        self.non_movable_objects = []
 
         for object in layout:
-            sprite = object[0]
+            sprite = object.sprite
             sprite_height = sprite.get_height()
             sprite_width = sprite.get_width()
             x_scale = sprite_width//tile_width
             y_scale = sprite_height//tile_height
-            y = map_height - (object[1] * tile_height) + dy
-            x = (object[3] * tile_width) + dx
-            rows = (object[2] - object[1]) // y_scale
-            columns = (object[4] - object[3]) // x_scale
-
+            y = map_height - (object.row_start * tile_height) + dy
+            x = (object.column_start * tile_width) + dx
+            rows = (object.row_end - object.row_start) // y_scale
+            columns = (object.column_end - object.column_start) // x_scale
 
             for row in range(rows):
                 y2 = y - (row * sprite_height)
@@ -110,16 +135,70 @@ class Foreground():
                     x2 = x + (column * sprite_width)
                     rect = sprite.get_rect(bottomleft=(x2, y2))
                     temp_tup = (sprite, rect)
-                    self.blit_list.append(temp_tup)
+
+                    if object.move_info != None:                    #move_info should be a tuple of (top_max, bottom_max, left_max, right_max, speed, (directions))
+                        self.movable_objects.append(temp_tup)
+                        self.move_infos.append(object.move_info)
+                    else:
+                        self.non_movable_objects.append(temp_tup)
+
+        self.blit_list = [self.movable_objects, self.non_movable_objects]
 
     def move(self, dx, dy):
         self.dx+=dx
         self.dy+=dy
-        for tup in self.blit_list:
-            tup[1].move_ip(dx, dy)
+        for list in self.blit_list:
+            for tup in list:
+                tup[1].move_ip(dx, dy)
+
+    def auto_move(self):
+        for i, tup in enumerate(self.movable_objects):
+            move_info = self.move_infos[i]
+            rect = tup[1]
+            bottom = move_info[0] * self.tile_height
+            top = move_info[1] * self.tile_height
+            left = move_info[2] * self.tile_width
+            right = move_info[3] * self.tile_width
+            speed = move_info[4]
+            directions = move_info[5]
+
+            if 'DOWN' in directions:
+                if rect.y > bottom:
+                    tup[1].move_ip(0, speed)
+                else:
+                    directions.append('UP')
+                    directions.remove('DOWN')
+
+            if 'UP' in directions:
+                if rect.y < top:
+                    tup[1].move_ip(0, -speed)
+                else:
+                    directions.append('DOWN')
+                    directions.remove('UP')
+
+            if 'RIGHT' in directions:
+                if rect.x < right :
+                    tup[1].move_ip(speed, 0)
+                else:
+                    directions.append('LEFT')
+                    directions.remove('RIGHT')
+
+            if 'LEFT' in directions:
+                if rect.x > left:
+                    tup[1].move_ip(-speed, 0)
+                else:
+                    directions.append('RIGHT')
+                    directions.remove('LEFT')
+
+
+
+
+
+
 
     def display(self):
-        screen.blits(self.blit_list)
+        for list in self.blit_list:
+            screen.blits(list)
 
 
 class Player():
@@ -163,28 +242,43 @@ class Player():
                 else:
                     foreground.move(0,-5)
                     background.move(0,-5)
-        print(foreground.dy)
 
 
 
-#class Object(pygame.sprite.Sprite):
-    #def __init__(self, sprite, x, y):
-        #self.sprite = sprite
-        #self.rect = sprite.get_rect(x,y)
-        #self.rect.x = x
-        #self.rect.y = y
+
+class Object():
+    def __init__(self, sprite, row_start, row_end, column_start, column_end, range_and_speed = None, solid = True):
+        self.sprite = sprite
+        self.row_start = row_start
+        self.row_end = row_end
+        self.column_start = column_start
+        self.column_end = column_end
+        self.move_info = range_and_speed
+        self.solid = solid
+
+
 
 
 
 #Below is test code.
-background = Background('background', 5, 0, 6, (1.8, 2.4))
-tiles = SpriteSheet('Tileset.png', 32, 32, (2,2))
-
-
+background = Background('background', 5, 0, 10, (1.8, 2.4))
+num=[]
 player = Player(0,0, 400,600)
-tile = tiles.get_large_sprite(1,2,2,4)
-platform = tiles.get_large_sprite(4, 6, 6, 8)
-foreground = Foreground(6000, 720, 64, 64, [[tile, 0,1,-80,80]])
+
+tiles = SpriteSheet('Tileset.png', 32, 32, (2,2))
+ground_tile = tiles.get_large_sprite(1,2,2,4)
+platform_sprite = tiles.get_large_sprite(3, 4, 5, 9)
+small_platform_sprite = tiles.join('H', [(3,5),(3,8)])
+
+ground = Object(ground_tile, 0, 1, -100, 100)
+platform1 = Object(platform_sprite, 3, 4, 6, 10)
+small_platform = Object(small_platform_sprite, 5, 6, 11, 13, (5, 6, 11, 15, 3, ['RIGHT']))
+
+
+foreground = Foreground(6000, 720, 64, 64, [ground, platform1, small_platform])
+#[platform, 3, 4, 6, 10, False],[small_platform, 5, 6, 11, 13, True]
+
+
 overlay=pygame.image.load('Overlay.png').convert_alpha()
 overlay.set_alpha(80)
 overlay=pygame.transform.scale(overlay,(display_width,display_height))
@@ -195,7 +289,8 @@ dx=0
 dy=0
 while run:
     movement = 'NONE'
-    #clock.tick(60)
+    foreground.auto_move()
+    clock.tick()
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
@@ -203,7 +298,7 @@ while run:
     screen.fill((255,255,255))
     background.display()
     screen.blit(tiles.get_sprite(1, 1), (player.dx, player.dy))
-    screen.blits(foreground.blit_list)
+    foreground.display()
     screen.blit(overlay,(0,0))
     pygame.display.update()
 
@@ -222,3 +317,10 @@ while run:
         movement = 'DOWN'
         player.update(movement)
 
+    num.append(clock.get_fps())
+
+fps=0
+for n in num:
+    fps+=n
+fps = fps/len(num)
+print(fps)
